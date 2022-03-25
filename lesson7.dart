d3e678b027f63bfa27ca26c77bf5d00b142f9cc5
Kaparovvv <kaparovkanagat@gmail.com>
2022-03-25 15:28:23 +0600

    Seventh commit

---
+++ lesson7.dart
@@ -0,0 +1,68 @@
+
+
+import 'dart:io';
+
+
+void main(List<String> args) {
+// 1
+  // showUser("Peter", 19);
+
+// 2
+  // List list = [32,45,654,23,45,67,78,2];
+  // showList(list);
+
+// 3
+  // stdout.write('Enter the a: ');
+  // int a  = int.parse(stdin.readLineSync()!);
+  // stdout.write('Enter the b: ');
+  // int b  = int.parse(stdin.readLineSync()!);
+  // triangle(a,b);
+  // String height = '*';
+  // String withs = '*';
+
+// 4
+  // heiwi(a, b);
+
+// 5
+  // stdout.write('Enter the a: ');
+  // int are  = int.parse(stdin.readLineSync()!);
+  // Peter(are);
+}
+
+// 1
+  // void showUser(String name, int age){
+  //   print('Ваше имя: ${name}\nВаш возраст: ${age}');
+  //   }
+
+// 2
+// void showList(List list){
+//   // for (int i=1; i<21; i++){
+//   //   list.add(i);
+//   //   print(list);
+//   // }
+//   print(list.first);
+//   print(list.last);
+// }
+
+// 3
+// void  triangle(int a, int b){
+//   int c = (a*a)+(b*b); 
+//   print(sqrt(c));
+// }
+
+// 4
+// void heiwi(int height, int withs){
+//   for (int i=1; i<=height; i++){
+//     print('*'*withs);
+//   }
+// }
+
+// 5
+  // void Peter(int air){
+  //   for (int i=0; i < air; i++){
+  //     if(i % 2 == 1){
+  //       print('${' ' * (air - i)} ${' *' * i}');
+  //     }
+  //   }
+  // }
+
